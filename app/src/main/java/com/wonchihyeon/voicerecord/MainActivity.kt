95b14e040a2e4207edd6ffd639379319f4c94243
ylose <heungeob1003@gmail.com>
2024-06-30 17:26:35 +0900

    음성 녹음 저장 파일 경로 지정

--- app/src/main/java/com/wonchihyeon/voicerecord/MainActivity.kt
+++ app/src/main/java/com/wonchihyeon/voicerecord/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.wonchihyeon.voicerecord
 
 import android.content.pm.PackageManager
+import android.media.MediaRecorder
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -16,12 +17,25 @@ class MainActivity : AppCompatActivity() {
     // 앱에서 필요로 하는 권한 배열
     private var permissions: Array<String> = arrayOf(android.Manifest.permission.RECORD_AUDIO)
 
+    // 녹음된 음성파일 이름
+    var voiceFileName: String = ""
+
+    // 음성 녹음을 위한 MediaRecorder
+    var mediaRecorder: MediaRecorder? = null
+
+    // 녹음이 시작되었는지 상태 변수
+    var isRecordStart = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         // 음성 녹음 권한 Runtime에 권한 요청
         ActivityCompat.requestPermissions(this, permissions, REQUEST_CODE_RECORD_AUDIO_PERMISSION)
+
+        // 저장될 음성 파일 위치 지정
+        voiceFileName = "${externalCacheDir!!.absolutePath}/voice_record.3gp"
+
     }
 
     // 권한 요청이 완료된 경우 호출되는 함수
